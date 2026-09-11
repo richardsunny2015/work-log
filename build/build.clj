c702cfe655aa33539f06577144232e353a4c6b1c
Richard Lanchez <richardsunny2015@gmail.com>
2026-09-11 11:34:29 -0400

    Add build namespace

---
+++ build/build.clj
@@ -0,0 +1,24 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'work-log)
+(def version "0.1.0")
+(def class-dir "target/classes")
+(def uber-file (format "target/%s-%s-standaloned.jar" (name lib) version))
+
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :ns-compile '[work-log.main]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           :main 'work-log.main}))
